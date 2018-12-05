dc13dee13caf80a611df07433161899cdec15f0a
Zeeshan Azmat <zeeshanazmat56@gmail.com>
2018-12-05 17:56:44 +0500

    messages and heroes service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +9,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  public heroes = HEROES;
+  public heroes = [];
   public hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -16,12 +17,17 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero = this.hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
